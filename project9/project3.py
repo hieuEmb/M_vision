f550fa120da1a49e423cd23972c38b11a2d31404
hieuEmb <hieu0353777534@gmail.com>
2024-09-04 14:35:00 +0700

    Pro9

---
+++ project9/project3.py
@@ -0,0 +1,66 @@
+﻿import cv2
+import numpy as np
+
+def rgb_to_xyz(rgb):
+    # Ma trận chuyển đổi từ RGB sang XYZ
+    rgb_to_xyz_matrix = np.array([[0.4124564, 0.3575761, 0.1804375],
+                                  [0.2126729, 0.7151522, 0.0721750],
+                                  [0.0193339, 0.1191920, 0.9503041]])
+
+    # Chuyển đổi RGB sang XYZ
+    xyz = np.dot(rgb_to_xyz_matrix, rgb)
+
+    return xyz
+
+def chuyen_hinh_rgb_sang_xyz(hinh_goc):
+    # Tạo một list để chứa 3 kênh ảnh tương ứng X-Y-Z
+    xyz_channels = []
+
+    # Mỗi kênh trong không gian màu XYZ được hiển thị bởi một hình ảnh
+    width, height = hinh_goc.shape[1], hinh_goc.shape[0]
+    X_channel = np.zeros((height, width), dtype=np.uint8)
+    Y_channel = np.zeros((height, width), dtype=np.uint8)
+    Z_channel = np.zeros((height, width), dtype=np.uint8)
+    XYZ_channel = np.zeros((height, width, 3), dtype=np.uint8)
+
+    for x in range(width):
+        for y in range(height):
+            # Lấy giá trị điểm ảnh tại vị trí (x, y)
+            B, G, R = hinh_goc[y, x]
+
+            # Chuyển đổi RGB sang XYZ
+            xyz_values = rgb_to_xyz(np.array([R, G, B]))
+
+            # Gán giá trị XYZ cho từng pixel trong ma trận
+            X_channel[y, x] = int(xyz_values[0])
+            Y_channel[y, x] = int(xyz_values[1])
+            Z_channel[y, x] = int(xyz_values[2])
+            XYZ_channel[y, x] = (int(xyz_values[2]), int(xyz_values[1]), int(xyz_values[0]))
+
+    # Add các kình tương ứng vào các kênh màu X-Y-Z vào list
+    xyz_channels.extend([X_channel, Y_channel, Z_channel, XYZ_channel])
+
+    # Hàm trả về một list 4 ảnh Bitmap tương ứng với 3 kênh màu X-Y-Z và ảnh kết hợp XYZ
+    return xyz_channels
+
+# Đường dẫn đến file hình
+file_hinh = r'C:\Users\Admin\Desktop\Thi_Giac\Bai_9\lena_color.png'
+
+# Đọc ảnh màu dùng thư viện OpenCV
+hinh_goc = cv2.imread(file_hinh)
+
+# Gọi hàm chuyển đổi RGB sang XYZ
+xyz_channels = chuyen_hinh_rgb_sang_xyz(hinh_goc)
+
+# Hiển thị ảnh gốc và từng kênh màu X, Y, Z, và ảnh kết hợp XYZ
+cv2.imshow('Original Image', hinh_goc)
+cv2.imshow('X Channel', xyz_channels[0])
+cv2.imshow('Y Channel', xyz_channels[1])
+cv2.imshow('Z Channel', xyz_channels[2])
+cv2.imshow('XYZ Channel', xyz_channels[3])
+
+# Bấm phím bất kỳ để đóng cửa sổ hiển thị ảnh
+cv2.waitKey(0)
+
+# Giải phóng bộ nhớ đã cấp phát cho các cửa sổ
+cv2.destroyAllWindows()
---
+++ project9/project9/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace project6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Load ảnh từ file
+            Bitmap HinhGoc = new Bitmap(@"D:\hk2nam3\thigiacmay\Project\Bai9\lena_color.png");
+
+            // Hiển thị lên picbox
+            pictureBox1.Image = HinhGoc;
+
+            // Hiển thị các kênh màu RGB được chuyển đổi từ XYZ
+            // Gọi hàm chuyển đổi RGB sang XYZ
+            List<Bitmap> xyzChannels = ChuyenHinh_RGB_Sang_XYZ(HinhGoc);
+
+            // Hàm trả về 3 kênh màu tương ứng giá trị theo thứ tự từ 0-2 là X-Y-Z
+            // Bây giờ mình cho hiển thị trên các picture
+            pictureBox2.Image = xyzChannels[0];
+            pictureBox3.Image = xyzChannels[1];
+            pictureBox4.Image = xyzChannels[2];
+            pictureBox5.Image = xyzChannels[3];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private List<Bitmap> ChuyenHinh_RGB_Sang_XYZ(Bitmap hinhGoc)
+        {
+            // Tạo một list để chứa 3 kênh ảnh tương ứng X- Y- Z
+            List<Bitmap> xyzChannels = new List<Bitmap>();
+
+            // Mỗi kênh trong không gian màu XYZ được hiển thị bởi một hình Bitmap
+            Bitmap XChannel = new Bitmap(hinhGoc.Width, hinhGoc.Height);
+            Bitmap YChannel = new Bitmap(hinhGoc.Width, hinhGoc.Height);
+            Bitmap ZChannel = new Bitmap(hinhGoc.Width, hinhGoc.Height);
+            Bitmap XYZChannel = new Bitmap(hinhGoc.Width, hinhGoc.Height);
+            for (int x = 0; x < hinhGoc.Width; x++)
+            {
+                for (int y = 0; y < hinhGoc.Height; y++)
+                {
+                    // Lấy điểm ảnh
+                    Color pixel = hinhGoc.GetPixel(x, y);
+                    double R = pixel.R;
+                    double G = pixel.G;
+                    double B = pixel.B;
+
+                    double[,] rgbToXyzMatrix = {
+                        {0.4124564, 0.3575761, 0.1804375},
+                        {0.2126729, 0.7151522, 0.0721750},
+                        {0.0193339, 0.1191920, 0.9503041}
+                    };
+
+                    // Tính toán giá trị màu sắc XYZ
+                    double X = rgbToXyzMatrix[0, 0] * R + rgbToXyzMatrix[0, 1] * G + rgbToXyzMatrix[0, 2] * B;
+                    double Y = rgbToXyzMatrix[1, 0] * R + rgbToXyzMatrix[1, 1] * G + rgbToXyzMatrix[1, 2] * B;
+                    double Z = rgbToXyzMatrix[2, 0] * R + rgbToXyzMatrix[2, 1] * G + rgbToXyzMatrix[2, 2] * B;
+
+                    // Hiển thị kết quả
+                    // Cho hiển thị
+                    XChannel.SetPixel(x, y, Color.FromArgb((int)X, (int)X, (int)X));
+                    YChannel.SetPixel(x, y, Color.FromArgb((int)Y, (int)Y, (int)Y));
+                    ZChannel.SetPixel(x, y, Color.FromArgb((int)Z, (int)Z, (int)Z));
+                    XYZChannel.SetPixel(x, y, Color.FromArgb((int)X, (int)Y, (int)Z));
+
+                }
+            }
+
+            // Add các kình tương ứng vào các kênh màu X-Y-Z vào list
+            xyzChannels.Add(XChannel);
+            xyzChannels.Add(YChannel);
+            xyzChannels.Add(ZChannel);
+            xyzChannels.Add(XYZChannel);
+            // Hàm trả về một list 3 ảnh Bitmap tương ứng với 3 kênh màu X-Y-Z
+            return xyzChannels;
+        }
+
+        private void lbl2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
